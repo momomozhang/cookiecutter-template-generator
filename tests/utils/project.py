6062e37d75ba740c3534b16a1de82a02d3ae499b
Mengni Zhang <monicamengni@gmail.com>
2025-06-17 16:41:06 +0200

    add initialize_git_repo() function so test_linting_passes() can check the files only in the newly generated directory

--- tests/utils/project.py
+++ tests/utils/project.py
@@ -1,12 +1,22 @@
 import json
 import subprocess
 from copy import deepcopy
+from pathlib import Path
 from typing import Dict
 
 from tests.consts import PROJECT_DIR
 
 
+def initialize_git_repo(repo_dir: Path):
+    """Initialize a git repository with an initial commit."""
+    subprocess.run(["git", "init"], cwd=repo_dir, check=True)
+    subprocess.run(["git", "branch", "-M", "main"], cwd=repo_dir, check=True)
+    subprocess.run(["git", "add", "--all"], cwd=repo_dir, check=True)
+    subprocess.run(["git", "commit", "-m", "'initial commit by pytest'"], cwd=repo_dir, check=True)
+
+
 def generate_project(template_values: Dict[str, str]):
+    """Generate a cookiecutter project from template values and return the project path."""
     template_values: Dict[str, str] = deepcopy(template_values)
     cookiecutter_config = {
         "default_context": template_values,
