c509483f879600715e85c1cb17797fe90103b51d
Mengni Zhang <monicamengni@gmail.com>
2025-06-20 13:08:58 +0200

    move tests under tests/funvtional_tests/ folder

--- tests/functional_tests/test_makefile.py
+++ tests/functional_tests/test_makefile.py
@@ -7,8 +7,9 @@ def test_linting_passes(project_dir: Path):
     subprocess.run(["make", "lint-ci"], cwd=project_dir, check=True)
 
 
-def test_tests_pass():
-    pass
+def test_tests_pass(project_dir: Path):
+    subprocess.run(["make", "install"], cwd=project_dir, check=True)
+    subprocess.run(["make", "test-wheel-locally"], cwd=project_dir, check=True)
 
 
 def test_install_succeeds():
