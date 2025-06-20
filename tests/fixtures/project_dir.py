cac5fd54734d03be8ec5a1d3ea56745836c1914f
Mengni Zhang <monicamengni@gmail.com>
2025-06-20 13:20:50 +0200

    add test_session_id to enable multiple workers run tests simutaneously with pytest-xdist

--- tests/fixtures/project_dir.py
+++ tests/fixtures/project_dir.py
@@ -1,26 +1,32 @@
 import shutil
 import subprocess
 from pathlib import Path
+from uuid import uuid4
 
 import pytest
 
-from tests.utils.project import (
-    generate_project,
-    initialize_git_repo,
-)
+from tests.utils.project import generate_project, initialize_git_repo
 
 
 @pytest.fixture(scope="session")
 def project_dir():
+    test_session_id: str = generate_test_session_id()
     template_values = {
-        "repo_name": "test-repo",
+        "repo_name": f"test-repo-{test_session_id}",
     }
     generated_repo_dir: Path = generate_project(template_values=template_values)
-    initialize_git_repo(repo_dir=generated_repo_dir)
-    subprocess.run(["make", "lint-ci"], cwd=generated_repo_dir, check=False)
-    yield generated_repo_dir
-    shutil.rmtree(path=generated_repo_dir)
+    try:
+        initialize_git_repo(repo_dir=generated_repo_dir)
+        subprocess.run(["make", "lint-ci"], cwd=generated_repo_dir, check=False)
+        yield generated_repo_dir
+    finally:
+        shutil.rmtree(path=generated_repo_dir)
 
     config_file = generated_repo_dir.parent.parent / "cookiecutter-test-config.json"
     if config_file.exists():
         config_file.unlink()
+
+
+def generate_test_session_id() -> str:
+    test_session_id = str(uuid4())
+    return test_session_id
